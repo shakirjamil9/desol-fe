dc6839c35d90f3fed5881324ae99b6197a515218
Shakir Jamil <mianshakir9@hotmail.com>
2024-12-12 21:26:27 +0500

    fix

--- utils/axiosInstance.js
+++ utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:4000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "https://desol-test-be.treebetpro.com/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
